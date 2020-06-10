43246c59ba4493cc431560b008a6623ea7169196
Richard Carino <richard.carino17@gmail.com>
2020-06-11 03:22:05 +1000

    Created 2 tests until this point

--- TechJobsTests/JobTests.cs
+++ TechJobsTests/JobTests.cs
@@ -13,7 +13,26 @@ namespace TechJobsTests
             var testJob2 = new Job();
 
             Assert.IsFalse(testJob1.Id == testJob2.Id);
-            //Compares IDs of both jobs, should return false
+            //Compares IDs of both jobs, should return false indicating that they do not have duplicate IDs
+        }
+
+        [TestMethod]
+        public void TestJobConstructorSetsAllFields()
+        {
+            string name = "Product Tester";
+            var employerName = new Employer("ACME");
+            var jobLocation = new Location("Desert");
+            var jobType = new PositionType("Quality Control");
+            var jobCoreCompetency = new CoreCompetency("Persistence");
+
+            var testJob = new Job(name, employerName, jobLocation, jobType, jobCoreCompetency);
+            Assert.IsTrue(
+                testJob.Id==1 &&
+                testJob.EmployerName==employerName &&
+                testJob.Name == name &&
+                testJob.JobType == jobType &&
+                testJob.JobCoreCompetency == jobCoreCompetency
+            );
         }
     }
 }
